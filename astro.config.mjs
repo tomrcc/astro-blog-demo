003efd79c11856d0a2cee3827d0c61e3b2f7af50
tomrcc <tom@cloudcannon.com>
2024-10-30 15:09:20 +1300

    Refactor posts collection to content collections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 
-import preact from "@astrojs/preact";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [preact()]
+  integrations: [react()],
 });
