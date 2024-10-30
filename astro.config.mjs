7e569fd8520bddd5c705e1741074d8eb6ce2dac9
tomrcc <tom@cloudcannon.com>
2024-10-30 15:26:16 +1300

    Add bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import bookshop from "@bookshop/astro-bookshop";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [react()],
+  integrations: [react(), bookshop()],
 });
